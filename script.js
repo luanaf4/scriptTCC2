64ec3ebf6a81d41abb88a13647e46ed33304899e
luanaf4 <103442333+luanaf4@users.noreply.github.com>
2025-09-26 22:47:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -586,7 +586,7 @@ class GitHubAccessibilityMiner {
       readmeContent.includes("lib") ||
       readmeContent.includes("gui") ||
       readmeContent.includes("graphical user interface") ||
-      readmeContent.includes("Turbo")
+      name === "turbo"
     ) {
       console.log(`   📚 Biblioteca/GUI detectada no README`);
       return true;
